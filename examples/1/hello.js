8cff13976f2b463a2c412ed4817cb80118c4d6e5
svarowskey <vladimir.svarowski@gmail.com>
2018-11-14 17:11:20 +0600

    Homework is ready.

--- examples/1/hello.js
+++ examples/1/hello.js
@@ -1,3 +1,22 @@
 "use strict";
-alert("Hello")
-alert("world")
+
+function task1() {
+    let temp_Cels = parseInt(prompt("Введите температуру в градусах по Цельсию: "));
+    if (!isNaN(temp_Cels)){
+    let temp_Far = ((9 / 5) * temp_Cels + 32);
+    alert("Температура по Фаренгейту равна: " + temp_Far);
+    } else {
+        alert("Введите корректное значение!")
+    }
+}
+
+function task2() {
+    let admin;
+    let name = "Василий";
+    admin = name;
+    alert("admin - " + admin);
+}
+
+function task3() {
+    alert('1000 + "108" = ' + (1000 + "108"));
+}
